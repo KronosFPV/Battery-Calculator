7d43e6f91bbe63d4f2423c12483d3db6bc85c38c
KronosFPV <137655816+KronosFPV@users.noreply.github.com>
2025-02-11 14:34:31 +0100

    Update BatteryCalculator.tsx

--- app/components/BatteryCalculator.tsx
+++ app/components/BatteryCalculator.tsx
@@ -180,7 +180,7 @@ const BatteryCalculator = () => {
                 onChange={(e) => updateOrder(order.id, 'cellType', e.target.value)}
                 className="w-full p-2 border rounded"
               >
-                {cellTypes[order.type].map(cellType => (
+                {cellTypes[order.type]?.map(cellType => (
                   <option key={cellType} value={cellType}>
                     {cellType}
                   </option>
