9d128f9e1c62e7a1cd97302cf65afa4adff697a0
KronosFPV <137655816+KronosFPV@users.noreply.github.com>
2025-02-11 13:41:54 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Battery Calculator',
+  description: 'Calculator for battery orders from China',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="de">
+      <body>{children}</body>
+    </html>
+  )
+}
